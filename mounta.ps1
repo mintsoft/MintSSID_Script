beb623a102d2e672335ee31bacfbe87eea0e3b98
Rob Emery <rob@mintsoft.net>
2013-05-25 20:32:44 +0100

    Rewriting parser to be much, much cleaner

--- mounta.ps1
+++ mounta.ps1
@@ -4,42 +4,22 @@
 #########################################################################################
 
 ## Load the supporting utils
-. "./mounta_utils.ps1"
+. "./mounta_utils.ps1";
 
-$lines = netsh wlan show interfaces 
-$filteredLines = $lines | where { $_ -match " : " }
-## Will need this: 
-##   http://social.technet.microsoft.com/Forums/en-CA/winserverpowershell/thread/4ac53d11-40ed-4c40-aab3-451ad502667e
-
-$parsed = $filteredLines -split " : "
-
-$objArray = @{}
-$key = ""; $val = ""; $x=1
-foreach ($line in $parsed)
-{
-	if($x -eq 1)
-	{
-		$key = $line.Trim()
-		$x=2
-	}
-	else
-	{
-		$val = $line.Trim()
-		$x=1
-		
-		$objArray[$key] = $val
-	}
+$wlan_info = @{};
+netsh wlan show interfaces | where { $_ -match " : " } | foreach {
+	$split_line = $_.Trim() -split " : ";
+	$wlan_info[$split_line[0]] = $split_line[1];
 }
 
 ## If connected to a wireless network, then run the named script
-
-if($objArray['State'] -eq "connected")
+if($wlan_info['State'] -eq "connected")
 {
 	# Run the "before" script
 	& "Scripts-defaults\pre.ps1";
 	
 	#check if file exists and run it
-	$scriptName = '.\Scripts-custom\'+$objArray['SSID']+'.ps1';
+	$scriptName = '.\Scripts-custom\'+$wlan_info['SSID']+'.ps1';
 	if(Test-Path $scriptName)
 	{
 		& $scriptName;
